b8d8584bff555c0d481b3eddc6fa5fba93395848
Danny <danny.v.der.pol@gmail.com>
2025-12-04 21:56:04 +0100

    add load content from URL

--- src/ki_reader.js
+++ src/ki_reader.js
@@ -5,14 +5,43 @@
 export class KiReader
 {
 
-    constructor(filename, debug)
+    constructor(filename, showDebug)
     {
         this.filename = filename;
-        this.debug = debug;
+        this.showDebug = showDebug;
     }
 
     async loadFile()
     {
-        if (this.debug) console.log('Reader: load file:', this.filename);
+        let content = {};
+        if (this.showDebug) console.log('Reader: load file:', this.filename);
+        try
+        {
+            const response = await fetch(this.filename);
+            if (!response.ok)
+            {
+                if (this.showDebug) console.error(`Reader: HTTP error status: ${response.status}`);
+            }
+            else
+            {
+                const text = await response.text();
+                content = this._parseFile(text);
+                content.filename = this.filename;
+                if (this.showDebug) console.log('Reader: file loaded successfully');
+            }
+        }
+        catch (error)
+        {
+            if (this.showDebug) console.error('Reader: error loading file:', error);
+        }
+        return content;
+    }
+
+    _parseFile(data)
+    {
+        let content = {
+        }
+
+        return content;
     }
 }

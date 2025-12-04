ce658fae6701083d7892e93e8c380bd02a8fea50
Danny <danny.v.der.pol@gmail.com>
2025-12-04 22:01:05 +0100

    reading sections from a KiCad file

--- src/ki_reader.js
+++ src/ki_reader.js
@@ -41,7 +41,61 @@ export class KiReader
     {
         let content = {
         }
-
+        let sections = this._getSections(data);
+        if (sections.length != 1)
+        {
+            throw new Error('Reader: invalid file format');
+        }
+        let value = this._getSectionName(sections[0]);
+        content.type = value;
+        // Get other sections
+        for (const section of this._getSections(sections[0].substring(1, sections[0].length - 1)))
+        {
+            console.log('section:', this._getSectionName(section));
+        }
         return content;
+    }
+
+    _getSections(data)
+    {
+        let sections = []
+        let start = -1
+        let section = 0
+        for (let i = 0; i < data.length; i++)
+        {
+            if (data[i] == '(')
+            {
+                if (section == 0)
+                {
+                    start = i;
+                }
+                section += 1
+            }
+            else if (data[i] == ')')
+            {
+                section -= 1
+            }
+            if (section == 0 && start >= 0 && i > start)
+            {
+                sections.push(data.substring(start,i + 1))
+                section = 0
+                start = -1
+            }
+        }
+        return sections;
+    }
+
+    _getSectionName(section)
+    {
+        let name = '';
+        for (let i = 2; i < section.length; i++)
+        {
+            if ([' ', '\r', '\n', '\t', ')'].includes(section[i]))
+            {
+                name = section.substring(1, i);
+                break;
+            }
+        }
+        return name.trim();
     }
 }

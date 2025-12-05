4b9fda7abd5d315b974038b02db960a3ac5ad412
Danny <danny.v.der.pol@gmail.com>
2025-12-05 19:55:21 +0100

    add setup to the drawer

--- src/ki_drawer.js
+++ src/ki_drawer.js
@@ -6,19 +6,35 @@ export class KiDrawer
 {
     constructor(ctx, scale, colors, showDebug)
     {
-        this.debug = showDebug;
+        this.showDebug = showDebug;
         this.colors = colors;
         this.ctx = ctx;
         this.scale = scale;
+        this.drawingType = null;
+        this.setup = null;
     }
 
     drawPageOutline(paperSize)
     {
-        if (this.debug) console.log('Drawer: drawing page outline for paper size:', paperSize);
+        if (this.showDebug) console.log('Drawer: drawing page outline for paper size:', paperSize);
         this.ctx.strokeStyle = this.colors.colorPageLimits;
         this.ctx.lineWidth = drawingDefaults.pageOutline / this.scale;
         this.ctx.strokeRect(0, 0, paperSize.width, paperSize.height);
         this.ctx.canvas.style.backgroundColor = this.colors.colorBackground;
+    }
+
+    drawContent(content)
+    {
+        this.drawingType = content.type;
+        if (this.showDebug) console.log('Drawer: Drawing type:', this.drawingType);
+        this.setup = content.setup;
+        this._drawObjects(content);
+        this.drawingType = null;
+    }
+
+    _drawObjects(objects)
+    {
+        console.log(objects);
     }
 }
 

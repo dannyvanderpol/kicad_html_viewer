ef4b1515f5c77b9f54af7f8ab4c8a361cf7307c8
Danny <danny.v.der.pol@gmail.com>
2025-12-07 15:09:18 +0100

    add pan and zoom

--- src/ki_viewer.js
+++ src/ki_viewer.js
@@ -17,11 +17,21 @@ class KiViewer
         this.content = null;
         this.sheet = null;
         this.colors = null;
+        this.pageSize = paperSizes.A4;
         this.viewportTransform = {
             x: 0,
             y: 0,
             scale: 1.0
         };
+        this.scaleSpeed = 1 / 250;
+        this.minScale = 1 / 50;
+        this.previousX = 0;
+        this.previousY = 0;
+        this.isMoving = false;
+        this.canvas.addEventListener('mousedown', this._onMouseDown.bind(this));
+        this.canvas.addEventListener('mouseup', this._onMouseUp.bind(this));
+        this.canvas.addEventListener('mousemove', this._onMouseMove.bind(this));
+        this.canvas.addEventListener('wheel', this._onMouseWheel.bind(this));
     }
 
     async initialize()
@@ -36,18 +46,22 @@ class KiViewer
 
         this.colors = new KiColors(this.content.type, this.debug & debugLevels.COLORS);
 
-        const pageSize = paperSizes[this.content.paper];
-        if (!pageSize)
+        this.pageSize = paperSizes[this.content.paper];
+        if (!this.pageSize)
         {
             if (this.debug & debugLevels.GENERAL) console.warn('Unknown page size:',  this.content.paper);
-            pageSize = paperSizes.A4;
+            this.pageSize = paperSizes.A4;
         }
 
         // Fit page to canvas
-        this.viewportTransform.scale = this.canvas.width / (pageSize.width * 1.02);
-        this.viewportTransform.x = (this.canvas.width - pageSize.width * this.viewportTransform.scale) / 2;
-        this.viewportTransform.y = (this.canvas.height - pageSize.height * this.viewportTransform.scale) / 2;
+        this.viewportTransform.scale = this.canvas.width / (this.pageSize.width * 1.02);
+        this.viewportTransform.x = (this.canvas.width - this.pageSize.width * this.viewportTransform.scale) / 2;
+        this.viewportTransform.y = (this.canvas.height - this.pageSize.height * this.viewportTransform.scale) / 2;
+        this._render();
+    }
 
+    _render()
+    {
         const ctx = this.canvas.getContext('2d');
 
         ctx.setTransform(1, 0, 0, 1, 0, 0)
@@ -62,8 +76,55 @@ class KiViewer
         );
 
         const drawer = new KiDrawer(ctx, this.viewportTransform.scale, this.colors, this.debug & debugLevels.DRAWER);
-        drawer.drawPageOutline(pageSize);
+        drawer.drawPageOutline(this.pageSize);
         drawer.drawContent(this.sheet);
+    }
+
+    /* Event handlers */
+    _onMouseDown(e)
+    {
+        if (this.debug & debugLevels.EVENTS) console.log('Mouse down:', e);
+        this.previousX = e.clientX;
+        this.previousY = e.clientY;
+        this.isMoving = true;
+    }
+
+    _onMouseUp(e)
+    {
+        if (this.debug & debugLevels.EVENTS) console.log('Mouse up:', e);
+        this.isMoving = false;
+    }
+
+    _onMouseMove(e)
+    {
+        if (!this.isMoving) return;
+        if (this.debug & debugLevels.EVENTS) console.log('Mouse move:', e);
+        const localX = e.clientX;
+        const localY = e.clientY;
+        this.viewportTransform.x += localX - this.previousX;
+        this.viewportTransform.y += localY - this.previousY;
+        this.previousX = localX;
+        this.previousY = localY;
+        this._render()
+    }
+
+    _onMouseWheel(e) {
+        if (this.debug & debugLevels.EVENTS) console.log('Mouse wheel:', e);
+        e.preventDefault();
+        const oldX = this.viewportTransform.x;
+        const oldY = this.viewportTransform.y;
+        const localX = e.clientX;
+        const localY = e.clientY;
+        const previousScale = this.viewportTransform.scale;
+        this.viewportTransform.scale += e.deltaY * -this.scaleSpeed;
+        console.log(previousScale, this.viewportTransform.scale);
+        if (this.viewportTransform.scale < this.minScale)
+        {
+            this.viewportTransform.scale = this.minScale;
+        }
+        this.viewportTransform.x = localX - (localX - oldX) * (this.viewportTransform.scale / previousScale);
+        this.viewportTransform.y = localY - (localY - oldY) * (this.viewportTransform.scale / previousScale);
+        this._render()
     }
 }
 

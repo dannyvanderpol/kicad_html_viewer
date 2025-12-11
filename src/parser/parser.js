99408df00b765bd3a2549c3115f36374e30599fe
Danny <danny.v.der.pol@gmail.com>
2025-12-11 21:31:39 +0100

    add parser function

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -2,10 +2,14 @@
  * Parsing a KiCad file into a design object.
  * input: filename (URI)
  * output: design object
- *
  */
 
-export class Parser
-{
+import { Design } from '../design/design.js';
+import { fetchFile } from '../lib/fetch_file.js';
 
+export async function parseFile(filename)
+{
+    let design = new Design();
+    const content = await fetchFile(filename);
+    return design;
 }

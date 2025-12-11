8b110e1b2e796cfd5d2b69391723556c7b769ff4
Danny <danny.v.der.pol@gmail.com>
2025-12-11 21:31:17 +0100

    add design object

---
+++ src/design/design.js
@@ -0,0 +1,14 @@
+/*
+ * Design object.
+ * Holds design objects and graphics objects.
+ */
+
+export class Design
+{
+    constructor()
+    {
+        this.fileType = null;
+        this.designObjects = [];
+        this.graphicsObjects = [];
+    }
+}

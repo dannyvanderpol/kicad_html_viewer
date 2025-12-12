c260d6abd936b02f89946527453af768c6665f16
Danny <danny.v.der.pol@gmail.com>
2025-12-12 19:31:36 +0100

    parse main section

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -7,16 +7,29 @@
 import { Design } from '../design/design.js';
 import { fetchFile } from '../lib/fetch_file.js';
 import { logger } from '../lib/logger.js';
+import { Sections } from './sections.js';
 
 export async function parseFile(filename)
 {
-    if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.time('Parser', `[Parser] Parsing file: '${filename}'`);
+    if (logger.logLevel & logger.LEVEL_PARSER_FETCH) logger.info('Fetcher', '[Parser] Fetching file content');
+    const content = await fetchFile(filename);
+    if (logger.logLevel & logger.LEVEL_PARSER_FETCH) logger.info('Fetcher', `[Parser] Content length: ${content.length}`);
+
+    if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.info('Parser', `[Parser] Parsing file: '${filename}'`);
     let design = new Design();
 
-    if (logger.logLevel & logger.LEVEL_PARSER_FETCH) logger.time('Fetcher', '[Parser] Fetching file content');
-    const content = await fetchFile(filename);
-    if (logger.logLevel & logger.LEVEL_PARSER_FETCH) logger.time('Fetcher', `[Parser] Content length: ${content.length}`);
+    let sections = Sections.getSections(content);
+    if (sections.length != 1)
+    {
+        if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.error('[Parser] Invalid file format.');
+    }
+    else
+    {
+        design.fileType = Sections.getSectionName(sections[0]);
+        if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.info(`[Parser] Design type: '${design.fileType}'`);
 
-    if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.time('Parser', '[Parser] Parsed design:', design);
+    }
+
+    if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.info('Parser', '[Parser] Parsed design:', design);
     return design;
 }

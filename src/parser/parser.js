8e0d615bce63fa47ee06c3f90e50d53ca41cefff
Danny <danny.v.der.pol@gmail.com>
2025-12-12 08:30:00 +0100

    add global logger

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -6,10 +6,17 @@
 
 import { Design } from '../design/design.js';
 import { fetchFile } from '../lib/fetch_file.js';
+import { logger } from '../lib/logger.js';
 
 export async function parseFile(filename)
 {
+    if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.time('parser', `[Parser] Parsing file: '${filename}'`);
     let design = new Design();
+
+    if (logger.logLevel & logger.LEVEL_PARSER_FETCH) logger.time('fetch', '[Parser] Fetching file content');
     const content = await fetchFile(filename);
+    if (logger.logLevel & logger.LEVEL_PARSER_FETCH) logger.time('fetch', `[Parser] Content length: ${content.length}`);
+
+    if (logger.logLevel & logger.LEVEL_PARSER_GENERAL) logger.time('parser', '[Parser] Parsed design:', design);
     return design;
 }

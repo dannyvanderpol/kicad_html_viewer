f70976c7edb66e709513ce7dee9b845158da00ef
Danny <danny.v.der.pol@gmail.com>
2025-12-13 20:43:58 +0100

    add colors object

---
+++ src/lib/colors.js
@@ -0,0 +1,9 @@
+/*
+ * Colors for elements.
+ * Based on the KiCad default colors.
+ * See: <user_folder>\AppData\Roaming\kicad\9.0\colors\user.json
+ */
+
+export const Colors = {
+    default: 'grey'
+}

0c2c383a4d3e994eeb126c1531543d448df176eb
Danny <danny.v.der.pol@gmail.com>
2025-12-13 20:34:40 +0100

    implement drawing polygon

--- src/graphics/polygon.js
+++ src/graphics/polygon.js
@@ -6,8 +6,17 @@ import { GraphicsBase } from './graphics_base.js';
 
 export class Polygon extends GraphicsBase
 {
-    drawElement()
+    drawElement(ctx)
     {
-
+        ctx.fillStyle = this.color;
+        ctx.lineWidth = 0;
+        ctx.beginPath();
+        ctx.moveTo(this.points[0].x, this.points[0].y);
+        for (let i = 1; i < this.points.length; i++)
+        {
+            ctx.lineTo(this.points[i].x, this.points[i].y);
+        }
+        ctx.closePath();
+        ctx.fill();
     }
 }

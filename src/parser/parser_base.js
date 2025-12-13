6599698e623b862de6b2ec5721de6fcca4162d2c
Danny <danny.v.der.pol@gmail.com>
2025-12-13 21:18:39 +0100

    add parsing color

--- src/parser/parser_base.js
+++ src/parser/parser_base.js
@@ -2,16 +2,21 @@
  * Base parser for all section parsers.
  */
 
-import { DesignObject } from '../design/design_object.js';
+'use strict';
+
+import { DesignElement } from '../design/design_element.js';
+import { Colors } from '../lib/colors.js';
+import { logger } from '../lib/logger.js';
 import { Sections } from './sections_parser.js';
 import { timer } from '../lib/timer.js';
 
 export class ParserBase
 {
-    constructor(sectionContent)
+    constructor(designType, sectionContent)
     {
-        this.designObject = new DesignObject;
-        this.graphicsObjects = [];
+        this.designType = designType;
+        this.designElement = new DesignElement;
+        this.graphicsElements = [];
         this.sectionName = Sections.getSectionName(sectionContent);
         timer.start(`Parse ${this.sectionName}`);
         this.sections = Sections.getSections(sectionContent.substring(1, sectionContent.length - 1));
@@ -23,5 +28,35 @@ export class ParserBase
     {
         // To be implemented by subclasses
         console.error('parseSection() not implemented in subclass');
+    }
+
+    getColor(identifier)
+    {
+        const defaultPcbAlpha = 0.7;
+        let color = Colors.default;
+        if (logger.logLevel & logger.LEVEL_PARSER_COLOR) logger.info('[ParserBase] Get color for:', this, identifier);
+        const colors = Colors[this.designType];
+        if (colors)
+        {
+            // Color for layers: B.Cu, F.Cu, etc.
+            let parts = identifier.toLowerCase().split('.');
+            if (parts.length == 2)
+            {
+                color = colors[parts[1]][parts[0]] || Colors.default;
+                if (color.startsWith('rgb('))
+                {
+                    color = color.replace('rgb', 'rgba').replace(')', `, ${defaultPcbAlpha})`);
+                }
+            }
+            else
+            {
+                color = colors[identifier] || Colors.default;
+            }
+        }
+        else
+        {
+            if (logger.logLevel & logger.LEVEL_PARSER_COLOR) logger.warn(`[ParserBase] No colors for ' ${this.designType}'`);
+        }
+        return color;
     }
 }

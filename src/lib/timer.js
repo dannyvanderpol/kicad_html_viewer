0173f01c85353a16f75d70b8c5c596ceb68e9571
Danny <danny.v.der.pol@gmail.com>
2025-12-16 19:58:36 +0100

    create report for new logger

--- src/lib/timer.js
+++ src/lib/timer.js
@@ -2,11 +2,20 @@
  * Timer for tracking elapsed time (singleton).
  */
 
+'use strict';
+
+import { logger } from "./logger.js";
+
 class Timer
 {
     #timers = {};
 
     constructor() {}
+
+    reset()
+    {
+        this.#timers = {};
+    }
 
     start(label)
     {
@@ -19,25 +28,41 @@ class Timer
         if (this.#timers[label] !== undefined)
         {
             const startTime = this.#timers[label].pop();
-            const elapsed = Math.round((performance.now() - startTime) * 10) / 10;
+            const elapsed = performance.now() - startTime
             this.#timers[label].push(elapsed);
         }
     }
 
     showReport()
     {
-        const report = {};
+        const lines = [];
+        let maxLabelLength = 0;
+        for (const [label, times] of Object.entries(this.#timers))
+        {
+            if (label.length > maxLabelLength) maxLabelLength = label.length;
+        }
+        maxLabelLength++;
+        lines.push('Timer results:');
+        let hLine = '+' + '-'.repeat(maxLabelLength + 1) + ('+' + '-'.repeat(11)).repeat(4) + '+';
+        lines.push(hLine);
+        let line = '| label'.padEnd(maxLabelLength + 2, ' ') + '| Calls (#) | Avg (ms)  | Min (ms)  | Max (ms)  |';
+        lines.push(line);
+        lines.push(hLine);
         for (const [label, times] of Object.entries(this.#timers))
         {
             const total = times.reduce((a, b) => a + b, 0);
-            report[label] = {
-                'Calls (#)': times.length,
-                'Avg (ms)': Math.round((total / times.length) * 10) / 10,
-                'Min (ms)': Math.min(...times),
-                'Max (ms)': Math.max(...times)
-            };
+            line = '| ' + label.padEnd(maxLabelLength) + '|';
+            line += ' ' + times.length.toString().padEnd(10, ' ') + '|';
+            line += ' ' + (total / times.length).toFixed(1).padEnd(10, ' ') + '|';
+            line += ' ' + Math.min(...times).toFixed(1).padEnd(10, ' ') + '|';
+            line += ' ' + Math.max(...times).toFixed(1).padEnd(10, ' ') + '|';
+            lines.push(line);
         }
-        console.table(report);
+        lines.push(hLine);
+        for (line of lines)
+        {
+            logger.info(line);
+        }
     }
 }
 

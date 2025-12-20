04616b843957227e82870c0c2b68bc201c3a1f7f
Danny <danny.v.der.pol@gmail.com>
2025-12-20 20:35:16 +0100

    add local test settings (not in git)

--- test/assets/test_page.js
+++ test/assets/test_page.js
@@ -2,19 +2,6 @@
 
 'use strict';
 
-// From src/lib/logger.js
-const LEVEL_SYSTEM = 0x01;
-const LEVEL_EVENTS = 0x02;
-const LEVEL_TIMER = 0x04;
-const LEVEL_VIEWER = 0x08;
-const LEVEL_PARSER = 0x10;
-const LEVEL_DRAWER = 0x20;
-
-const kiViewerSettings = {
-    // Show additional log levels:
-    logLevel: 0
-}
-
 const PAGE_CONTENT = `<p>KiCad Schematic:</p>
 <canvas src="designs/all_features/all_features.kicad_sch" type="application/kicad" width="900" height="640"></canvas>
 

c2e8a0a82f9fbe5902732583a2edb5958175106b
Danny <danny.v.der.pol@gmail.com>
2025-12-22 18:58:24 +0100

    add setting log level for test page

--- pages/assets/test_page.js
+++ pages/assets/test_page.js
@@ -2,7 +2,8 @@
 
 'use strict';
 
-const COOKIE_KEY = 'canvas_src';
+const COOKIE_KEY_SRC = 'canvas_src';
+const COOKIE_KEY_LOG = 'log_level';
 
 const kiViewerSettings =
 {
@@ -12,13 +13,16 @@ const kiViewerSettings =
 function loadSettings()
 {
     let filename = '';
-    let canvasSrc = getCookie(COOKIE_KEY);
+    let canvasSrc = getCookie(COOKIE_KEY_SRC);
+    let logLevel = getCookie(COOKIE_KEY_LOG);
+
     let elmProject = document.getElementById('project');
+    let elmLogLevel = document.getElementById('log-level');
 
     if (canvasSrc.startsWith('local_storage:'))
     {
         filename = canvasSrc.split(':')[1];
-        canvasSrc = localStorage.getItem(COOKIE_KEY);
+        canvasSrc = localStorage.getItem(COOKIE_KEY_SRC);
     }
     if (!canvasSrc)
     {
@@ -32,12 +36,26 @@ function loadSettings()
     document.getElementById('filename').innerHTML = filename;
     document.getElementById('kicad-design').setAttribute('src', canvasSrc);
     document.getElementById('kicad-design').setAttribute('filename', filename);
+
+    if (!logLevel)
+    {
+        logLevel = 0;
+        if (elmLogLevel)
+        {
+            logLevel = elmLogLevel.value;
+        }
+    }
+    if (elmLogLevel)
+    {
+        elmLogLevel.value = logLevel;
+    }
+    kiViewerSettings.logLevel = logLevel;
 }
 
 function selectProject()
 {
     let project = document.getElementById('project').value;
-    setCookie(COOKIE_KEY, project, 100);
+    setCookie(COOKIE_KEY_SRC, project, 100);
     location.reload();
 }
 
@@ -49,13 +67,20 @@ function uploadFile(event)
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function () {
-        localStorage.setItem(COOKIE_KEY, reader.result);
-        setCookie(COOKIE_KEY, 'local_storage:' + file.name, 100);
+        localStorage.setItem(COOKIE_KEY_SRC, reader.result);
+        setCookie(COOKIE_KEY_SRC, 'local_storage:' + file.name, 100);
         location.reload();
     };
     reader.onerror = function () {
         console.error('Error reading file:', reader.error);
     };
+}
+
+function setLogLevel()
+{
+    let logLevel = document.getElementById('log-level').value;
+    setCookie(COOKIE_KEY_LOG, logLevel, 100);
+    location.reload();
 }
 
 const PAGE_CONTENT = `

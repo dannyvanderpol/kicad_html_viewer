4efcc8cc0ef630dfca7b0e3b305c3958f91b9433
Danny <danny.v.der.pol@gmail.com>
2025-12-23 20:51:12 +0100

    fix junction size (radius vs diameter)

--- src/graphics/junction.js
+++ src/graphics/junction.js
@@ -9,7 +9,7 @@ export class Junction extends GraphicsBase
     drawElement(ctx)
     {
         ctx.beginPath();
-        ctx.arc(this.points[0].x, this.points[0].y, this.size, 0, Math.PI * 2);
+        ctx.arc(this.points[0].x, this.points[0].y, this.size / 2, 0, Math.PI * 2);
         ctx.fillStyle = this.color;
         ctx.fill();
     }

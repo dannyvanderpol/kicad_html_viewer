d3b86665877eb3829e54a1169d1798c90d8fb15e
Danny <danny.v.der.pol@gmail.com>
2025-12-24 20:08:31 +0100

    added logic for selecting the proper parser

--- src/parser/section_to_parser.js
+++ src/parser/section_to_parser.js
@@ -2,23 +2,27 @@
  * Select the proper parser for the given section.
  */
 
-// import { BusEntryParser } from './bus_entry_parser.js';
-// import { EmbeddedFileParser } from './embedded_file_parser.js';
-// import { JunctionParser } from './junction_parser.js';
-// import { LayersParser } from './layers_parser.js';
-// import { LineParser } from './line_parser.js';
-// import { PaperParser } from './paper_parser.js';
-// import { RectangleParser } from './rectangle_parser.js';
-// import { SetupParser } from './setup_parser.js';
-// import { TextParser } from './text_parser.js';
-// import { TitleBlockParser } from './title_block_parser.js';
-// import { ZoneParser } from './zone_parser.js';
+import { logger } from '../lib/logger.js';
 
 export function getParser(sectionName)
 {
-    return null;
+    if (skipSections.includes(sectionName))
+    {
+        return null;
+    }
+    const parserClass = sectionToParser[sectionName];
+    if (!parserClass)
+    {
+        logger.warn(logger.LEVEL_PARSER, '[Parser] Unknown section:', sectionName);
+        return null;
+    }
+    return new parserClass();
 }
 
-const SectionToParser = {
+const skipSections = [
 
-}
+];
+
+const sectionToParser = {
+
+};

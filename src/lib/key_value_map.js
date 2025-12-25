6c2e975fb92ffd27207dbf3eadfccba14f128eda
Danny <danny.v.der.pol@gmail.com>
2025-12-25 14:34:56 +0100

    add sheet name key

--- src/lib/key_value_map.js
+++ src/lib/key_value_map.js
@@ -21,6 +21,7 @@ export class KeyValueMap {
             'revision': '',
             'sheetpath': '/',
             'title': '',
+            'sheetname': '',
             '#': '',
             '##': ''
         };
